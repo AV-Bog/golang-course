061ae8d317bd27db26bf7efaf6cd6dc0253aab76
bogdanovaarina <arinabogdanova1967@mail.ru>
2026-04-09 18:42:41 +0300

    Fix что-то там 5

--- task3/repo-stat/api/internal/controller/http/routes.go
+++ task3/repo-stat/api/internal/controller/http/routes.go
@@ -1,12 +1,20 @@
 package http
 
 import (
+	"encoding/json"
 	"log/slog"
 	"net/http"
+
 	"repo-stat/api/internal/usecase"
 )
 
 func AddRoutes(mux *http.ServeMux, log *slog.Logger, ping *usecase.Ping, getRepo *usecase.GetRepository) {
+	mux.HandleFunc("GET /health", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(map[string]string{"status": "ok"})
+	})
+
 	mux.Handle("GET /api/ping", NewPingHandler(log, ping))
 	mux.Handle("GET /api/repositories/info", NewRepositoryHandler(log, getRepo))
 }
